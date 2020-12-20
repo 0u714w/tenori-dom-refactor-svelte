59fd7d2b771a1e2e6e5a364419a11291b5011f69
Lockett Pundt <kalifornium@hotmail.com>
2020-12-19 21:24:56 -0500

    feat(Save states): :sparkles: updated the save component to open a modal for a name ot be entered. it returns the name and Id for the user to record for retrieving the setting.

--- src/SaveSetting.svelte
+++ src/SaveSetting.svelte
@@ -1,8 +1,14 @@
 <script>
   import { mutation } from '@urql/svelte';
   import { getContext } from 'svelte';
+  import Modal from './Modal.svelte';
 
   const context = getContext('value');
+  const nameContext = getContext('name');
+  let open = false;
+  let settingName = '';
+  let errors = {};
+  let successfulResult = false;
 
   const newSettingMutation = mutation({
     query: `
@@ -18,13 +24,28 @@
     `,
   });
 
-  const saveSetting = async () => {
-    console.log($context);
-    const value = JSON.stringify($context);
-    const result = await newSettingMutation({
-      setting: { name: 'First New Setting', value },
-    });
-    console.log('here is the mutation result', result);
+  const saveSetting = async (name) => {
+    errors = {};
+    if (name.trim().length < 2) {
+      errors['name'] = 'Must be at least 2 characters.';
+    }
+    if (Object.values(errors).length === 0) {
+      const value = JSON.stringify($context);
+      const result = await newSettingMutation({
+        setting: { name, value },
+      });
+      console.log('here is the mutation result', result);
+      open = false;
+      settingName = '';
+      if (result?.data?.createNewSetting?.setting) {
+        const { id } = result.data.createNewSetting.setting;
+        nameContext.update(() => name);
+        successfulResult = {
+          name,
+          id,
+        };
+      }
+    }
   };
 </script>
 
@@ -36,6 +57,38 @@
     outline: none;
     cursor: pointer;
   }
+
+  span {
+    background-color: red;
+    border-radius: 3px;
+    color: black;
+    font-size: 0.8em;
+    justify-self: center;
+  }
+
+  .result {
+    display: grid;
+    gap: 10px;
+    grid-template-columns: 1fr;
+  }
 </style>
 
-<button on:click={(e) => saveSetting(e)}><i class="fas fa-save" /></button>
+<button on:click={() => (open = !open)}><i class="fas fa-save" /></button>
+<Modal bind:open>
+  <div>
+    <label for="name"> Name Your Setting </label>
+    <input type="text" id="name" placeholder="Moonlight Sonata" bind:value={settingName} />
+    <button on:click={() => saveSetting(settingName)}>Submit</button>
+    {#each Object.values(errors) as error}<span>{error}</span>{/each}
+  </div>
+</Modal>
+
+<Modal open={!!successfulResult}>
+  <div class="result">
+    <div>
+      Saved sucessfully! If you'd like to recall this at a later time, save the name and id.
+    </div>
+    <div>{successfulResult.name}</div>
+    <div>{successfulResult.id}</div>
+  </div>
+</Modal>
